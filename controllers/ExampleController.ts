6b0733145e900e3dc794bc5591d1d50d71c72033
IANONA Innocent <ianona.innocent>
2022-07-07 23:11:01 +0100

    TASK-1002

--- controllers/ExampleController.ts
+++ controllers/ExampleController.ts
@@ -21,6 +21,31 @@ export default class ExampleController {
                 res.status(503).send();
             }
         })
+        router.get('/api/timemachine/logs/mcfly', async(req,res)=>{
+            const response = {
+                js:[
+                    {
+                        name: "My mom is in love with me",
+                        version: "1.0",
+                        time: -446723100,
+                },{
+                    name: "I go to the future and my mom end up with the wrong guy",
+                    version: "2.0",
+                    time: 1445470140
+                },{
+                    name: "I go to the past and you will not believe what happens next",
+                    version: "3.0",
+                    time: -Date.now()
+                }]
+                    
+                
+            }
+            try {
+                res.send(response) 
+             } catch (error) {
+                 res.status(503).send();
+             } 
+        })
     }
 
     async sayHello(): Promise<string> {
